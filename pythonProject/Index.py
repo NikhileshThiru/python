51cc906b8b0648f16feca0d98886f3cf8c76779e
Nikhilesh <nikhilesh.thiru@gmail.com>
2021-07-07 12:32:50 -0400

    1st python project.

---
+++ pythonProject/Index.py
@@ -0,0 +1,9 @@
+name = "Nikhilesh Thiru"
+
+first_name = name[:9].upper()
+last_name = name[10:].lower()
+last_character = name[-1]
+
+print(first_name)
+print(last_name)
+print(last_character)
---
+++ pythonProject/Keyword_Arguments.py
@@ -0,0 +1,5 @@
+def hello(first, middle, last):
+    print("Hello "+first+" "+middle+" "+last)
+
+
+hello(last="Code", middle="Dude", first="Bro")
