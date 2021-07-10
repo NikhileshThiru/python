0d910a642fe2550da9eca4eed0497a3038397eca
Nikhilesh <nikhilesh.thiru@gmail.com>
2021-07-10 11:49:51 -0400

    1st python project.

---
+++ pythonProject/Discord_About_Me_Class.py
@@ -0,0 +1,14 @@
+class Shadowclone14:
+
+    def tired(self):
+        print("He will sleep.")
+
+    def bored(self):
+        print("He will play Minecraft or go outside.")
+
+    def hungry(self):
+        print("He will eat something.")
+
+
+Shadowclone14 = Shadowclone14()
+Shadowclone14.bored()
--- pythonProject/Test.py
+++ pythonProject/Test.py
@@ -1,5 +1,15 @@
-from tkinter import *
-import random
+import http.client
 
-window = Tk()
-window.title("Tick_Tac_Toe")
+conn = http.client.HTTPSConnection("community-open-weather-map.p.rapidapi.com")
+
+headers = {
+    'x-rapidapi-key': "a9e7ceb28fmshfabc38145db2c10p13a2bejsn4043a271ab47",
+    'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com"
+    }
+
+conn.request("GET", "/find?q=london&cnt=0&mode=null&lon=0&type=link%2C%20accurate&lat=0&units=imperial%2C%20metric", headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+
+print(data.decode("utf-8"))
